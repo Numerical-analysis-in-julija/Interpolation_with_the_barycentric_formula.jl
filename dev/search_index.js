ed46e970be0cbed94428b3e7c7a6150beb5e935d
Documenter.jl <documenter@juliadocs.github.io>
2023-07-02 19:26:44 +0000

    build based on b6fa846

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Interpolation*with*the*barycentric*formula.jl","page":"Interpolationwiththebarycentricformula.jl","title":"Interpolationwiththebarycentricformula.jl","text":"","category":"section"},{"location":"","page":"Interpolationwiththebarycentricformula.jl","title":"Interpolationwiththebarycentricformula.jl","text":"Documentation for Interpolationwiththebarycentricformula.jl","category":"page"}]
+}
